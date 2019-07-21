a6d231eaca53765c57278a433bf114a358b32d09
StarshipladDev <StarshipladDev@gmail.com>
2019-07-21 20:34:50 +1200

    Initial Upload
    Simple upload that allows you to draw violet dots on a form. Form has a button that connects to a database and reads the passage with ID=1.
    Will release development log with next update

---
+++ MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace FiveMoveMurderFest
+{
+    //Code just straight stolen from
+    //https://sqlchoice.azurewebsites.net/en-us/sql-server/developer-get-started/csharp/win/step/2.html
+    // and http://www.java2s.com/Code/CSharp/2D-Graphics/Draganddraw.htm
+    //
+    partial class MainForm
+    {
+
+        private Button button1;
+        private TextBox textBox1;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        bool shouldPaint = false;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-2, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Text = "Not RUnning";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Painter";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PainterForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PainterForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PainterForm_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Running";
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+           
+            builder.DataSource = "localhost";
+            builder.UserID = //FakeUserID
+            builder.Password = //Fakepassword
+            builder.InitialCatalog =//FakeinitialDB
+            //possible value: u963414567_db1
+
+            // Connect to SQL
+            Console.Write("Connecting to SQL Server ... ");
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Done.");
+
+                    // Create a sample database
+                    Console.ReadKey(true);
+                    String sql = "SELECT ID,Name,Info FROM ImportantInfo WHERE ID=1;";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                String print = reader.GetInt32(0) + " " + reader.GetString(1) + " " + reader.GetString(2);
+                                textBox1.Text = print;
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ev)
+            {
+                textBox1.Text=(ev.Message);
+            }
+            
+        }
+            private void PainterForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            shouldPaint = true;
+        }
+
+        private void PainterForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            shouldPaint = false;
+        }
+
+        private void PainterForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (shouldPaint)
+            {
+                Graphics graphics = CreateGraphics();
+                graphics.FillEllipse(new SolidBrush(Color.BlueViolet), e.X, e.Y, 4, 4);
+                graphics.Dispose();
+            }
+        }
+
+        #endregion
+
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiveMoveMurderFest
+{
+    public partial class MainForm : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
